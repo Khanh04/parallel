b2ea2f549302f9007a61084363877a58ffe5eea3
Khanh Nguyen <khanhng@umich.edu>
2024-05-18 17:19:32 +0300

    refactor finalized

--- dependency_graph.h
+++ dependency_graph.h
@@ -5,65 +5,7 @@
 #include <vector>
 #include <set>
 #include <iostream>
-
-class Var {
-public:
-    Var();
-    Var(std::string varName);
-    void printDetailed();
-    bool operator==(Var &v);
-    friend std::ostream& operator<<(std::ostream &out, Var &v);
-    void setRead(int statementId);
-    void setWrite(int statementId);
-    void setName(std::string n);
-    std::string getName();
-    int nameLength();
-    friend class Variables;
-private:
-    static int maxId;
-    int id;
-    int type;
-    std::vector<int> read;
-    std::vector<int> write;
-    std::string name;
-};
-
-class Variables {
-public:
-    void print();
-    void printDetailed();
-    Var* findVar(const std::string &s);
-    std::vector<Var> vars;
-    static std::vector<Variables> varSet;
-    static int iCurrentVarSet;
-private:
-};
-
-class Dependency {
-public:
-    Dependency(Var &variable, Var &dependsOn, int statementId);
-    void printStatementIds();
-    void print();
-    void addIndex(int statementId);
-    bool operator==(Dependency dep);
-    bool statementIdInStatementRangeExists(int min, int max);
-private:
-    Var _variable;
-    Var _dependsOn;
-    std::vector<int> statementIds;
-};
-
-class Graph {
-public:
-    Graph();
-    void addDependency(Var &var, Var &dependsOn, int statementId);
-    void print();
-    void printTable();
-    std::vector<Dependency> dependencies;
-    static std::vector<Graph> graphs;
-    static int iCurrentGraph;
-private:
-};
+#include "variables.h"
 
 void updateGraph(const int maxStatementId, std::string lhsVar, std::vector<std::string> &dependsOnList);
 
--- utils.cpp
+++ utils.cpp
@@ -15,26 +15,3 @@ std::string to_string(double x)
     ost << x;
     return ost.str();
 }
-
-template<int N>
-class Error {
-public:
-    explicit Error(const std::string s) : message{s} { }
-    
-    std::string get_message() const { return message; }
-    void put(std::ostream& os) const { os << message; }
-    
-private:
-    std::string message;
-};
-
-using Lexical_error = Error<1>;
-using Syntax_error = Error<2>;
-using Runtime_error = Error<3>;
-
-template<int N>
-std::ostream& operator<<(std::ostream& os, const Error<N>& e)
-{
-    e.put(os);
-    return os;
-}
