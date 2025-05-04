761563ae0b1b30d1f75211eaefbd26979931c630
Khanh Nguyen <khanhng@umich.edu>
2025-05-04 09:21:34 +0300

    update test.cpp

--- test.cpp
+++ test.cpp
@@ -10,17 +10,13 @@ int f(int aa, int bb){
 
 int main(){
     int a = 4, b = 2, p = 3, i;
-    int result, d;
+    int result, d=0;
     int arr[5];
     b = a+p;
     result = f(a,b);
     for(i=1;i<5;++i){ 
-        d=p+2;
-        arr[i] = arr[i-1];
-        arr[i-1] = d + 1;
-    }
-    while (a < 10) {
-        a=a+1;
+        d=d+2;
+        std::cout << "d =" << d << std::endl;
     }
     b = a*p;
 }
